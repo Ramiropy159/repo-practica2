228e501c51e8edf5b2cbed5406b8451943269abc
La R <ramitarios159@gmail.com>
2024-03-20 16:28:43 -0300

    se elimina bitcoin.txt, modifica texto.txt y add conversor-grados-radianes.py

---
+++ conversor-grados-radianes.py
@@ -0,0 +1,17 @@
+#inicio este archivo de pruebas desde la computadora del trabajo
+#paso previo a este fue abrir vscode de forma online y conectar a GitHub el mismo 
+#uso este repositorio para aprender como usar GitHub y familiarizarme como el mio 
+# Dudas
+# 1- ahora mi repositorio de github se actualiza solo o tengo que hacer algo ? - hay una A de agregado
+# 
+# 
+
+
+print('--- CONVERSOR DE GRADOS EN RADIANES ---')
+#decalro e inicializao variables a usar en el calculo
+grados = 0, pi = 3.14
+#pido al usuario que ingrese los grados
+input(var1)
+#hago el calculo 
+resultadoEnRadianes = (var1 * pi)/180
+print(grados,' grados equivalen a ',resultadoEnRadianes,'Condigo de prueba')
